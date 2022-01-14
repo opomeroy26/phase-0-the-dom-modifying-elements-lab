445a9f547862f6d65af7684950b3bdb19c0dd513
Olivia Pomeroy <opomeroy26@gmail.com>
2022-01-14 12:24:30 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+main.remove(main);
+const newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.id = 'victory';
+victory.textContent = "Olivia is the champion";
